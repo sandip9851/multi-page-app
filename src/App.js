adbb0610dd583fc9d366fa4414a5bcb99a629386
sandip <mr.sandippradhan@gmail.com>
2024-08-03 18:42:04 +0530

    correction routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './component/HomePage';
 import DetailPage from './component/DetailPage';
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <Router>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/post/:id" element={<DetailPage />} />
+                <Route path="/" Component={<HomePage />} />
+                <Route path="/post/:id" Component={<DetailPage />} />
             </Routes>
         </Router>
 
